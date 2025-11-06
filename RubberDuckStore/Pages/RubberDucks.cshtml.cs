8aea66098064271fe0953548be041a8b62153e97
rosemorenov <rose.morenov@gmail.com>
2025-11-06 01:10:17 +0000

    added code comments

--- RubberDuckStore/Pages/RubberDucks.cshtml.cs
+++ RubberDuckStore/Pages/RubberDucks.cshtml.cs
@@ -17,7 +17,7 @@ namespace RubberDuckStore.Pages
 
         // List that will hold all ducks for the dropdown selection
         public List<SelectListItem> DuckList { get; set; }
-        
+
         // Property that will store the currently selected duck object
         public Duck SelectedDuck { get; set; }
 
@@ -69,7 +69,7 @@ namespace RubberDuckStore.Pages
 
         // Helper method that retrieves a specific duck by its ID from the database
         // Returns all details of the duck
-        private Duck GetDuckById(int id)
+        private Duck? GetDuckById(int id)
         {
             using (var connection = new SqliteConnection("Data Source=RubberDucks.db"))
             {
@@ -77,8 +77,11 @@ namespace RubberDuckStore.Pages
                 var command = connection.CreateCommand();
                 command.CommandText = "SELECT * FROM Ducks WHERE Id = @Id";
                 command.Parameters.AddWithValue("@Id", id); // Using parameterized query for security
+
+                // Get a reader to read the resultset from the database
                 using (var reader = command.ExecuteReader())
                 {
+                    // If reader returns a record, then we'll return that new duck from the database
                     if (reader.Read())
                     {
                         return new Duck
@@ -89,21 +92,27 @@ namespace RubberDuckStore.Pages
                             Price = reader.GetDecimal(3),
                             ImageFileName = reader.GetString(4)
                         };
-                    }
-                }
-            }
+                    } // end if
+                } // end using
+            } // end using
+
+            // if there was no duck matching the ID in the database return null
             return null;
-        }
+        } // end get duck helper method
     }
 
 
     // Simple model class representing a rubber duck product
+    // model class maps to a table in the database so that y9ou can create objects
+    // in your application that represent t he intance of an entity (rows in the table)
+    // in the database
     public class Duck
     {
+        // The duck's attributes match the columns in the duck table
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageFileName { get; set; }
-    }
+    } // end duck class
 }    
